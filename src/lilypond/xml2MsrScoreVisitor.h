3c31fad62fb30f43f8cbf21e8373a32d768c6bf6
Jacques Menu <imj-muzhic@bluewin.ch>
2016-10-27 21:25:37 +0200

    part group 9

--- src/lilypond/xml2MsrScoreVisitor.h
+++ src/lilypond/xml2MsrScoreVisitor.h
@@ -551,6 +551,43 @@ class EXP xml2MsrScoreVisitor :
 
 };
 
+/*
+    The part-group element indicates groupings of parts in the
+    score, usually indicated by braces and brackets. Braces
+    that are used for multi-staff parts should be defined in
+    the attributes element for that part. The part-group start
+    element appears before the first score-part in the group.
+    The part-group stop element appears after the last
+    score-part in the group.
+    
+    The number attribute is used to distinguish overlapping
+    and nested part-groups, not the sequence of groups. As
+    with parts, groups can have a name and abbreviation.
+    Formatting attributes for group-name and group-abbreviation
+    are deprecated in Version 2.0 in favor of the new
+    group-name-display and group-abbreviation-display elements.
+    Formatting specified in the group-name-display and
+    group-abbreviation-display elements overrides formatting
+    specified in the group-name and group-abbreviation
+    elements, respectively.
+
+    The group-symbol element indicates how the symbol for
+    a group is indicated in the score. Values include none,
+    brace, line, bracket, and square; the default is none.
+    The group-barline element indicates if the group should
+    have common barlines. Values can be yes, no, or
+    Mensurstrich. The group-time element indicates that the
+    displayed time signatures should stretch across all parts
+    and staves in the group. Values for the child elements
+    are ignored at the stop of a group.
+
+    A part-group element is not needed for a single multi-staff
+    part. By default, multi-staff parts include a brace symbol
+    and (if appropriate given the bar-style) common barlines.
+    The symbol formatting for a multi-staff part can be more
+    fully specified using the part-symbol element, defined in
+    the attributes.mod file.
+*/
 
 /*! @} */
 
