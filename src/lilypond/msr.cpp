1c6ffeff98d55ca710a4e952730909be6e18bae3
jacques-menu <imj-219@bluewin.ch>
2017-02-14 14:49:33 +0100

    anacrusis 7

--- src/lilypond/msr.cpp
+++ src/lilypond/msr.cpp
@@ -7660,8 +7660,8 @@ void msrMeasure::print (ostream& os)
       ", " << fMeasureTime->timeAsString () <<
       ", len: " << getMeasureLength () <<
       " (" << getMeasureLengthAsString () << ")" <<
-      ", pwn:" << fMeasureDivisionsPerWholeNote <<
       ", pfm:" << fMeasureDivisionsPerFullMeasure <<
+      ", pwn:" << fMeasureDivisionsPerWholeNote <<
       ", pos: " << fMeasurePosition << 
       ", " << fMeasureElementsList.size () << " elems" <<
       ", " << getMeasureKindAsString () <<
@@ -9113,6 +9113,67 @@ void msrVoice::forceVoiceMeasureNumberTo (int measureNumber) // JMI
 void msrVoice::setNewSegmentForVoice (
   int inputLineNumber)
 {
+  // fetch segment last measure
+  S_msrMeasure
+    lastMeasure =
+      fSegmentMeasuresList.back ();
+
+  // fetch its last measure position and length
+  int
+    lastMeasureNumber =
+      lastMeasure->
+        getMeasureNumber (),
+        
+    lastMeasurePosition =
+      lastMeasure->
+        getMeasurePosition (),
+      
+    lastMeasureLength =
+      lastMeasure->
+        getMeasureLength (),
+      
+    lastMeasureDivisionsPerFullMeasure =
+      lastMeasure->
+        getMeasureDivisionsPerFullMeasure ();
+
+  // is the last measure full? (positions start at 1)
+  if (lastMeasurePosition <= lastMeasureDivisionsPerFullMeasure) {
+    // no, register last measure as incomplete
+    
+    if (gGeneralOptions->fTrace) {
+      cerr <<
+        idtr <<
+          "Measure " << measureNumber <<
+          " of segment " << fSegmentAbsoluteNumber <<
+          " in voice \"" <<
+          getSegmentVoiceUplink ()->getVoiceName () << "\"" <<
+          " is " <<
+          string(
+            lastMeasurePosition == 1
+              ? "incomplete"
+              : "empty") <<
+          ", line " << inputLineNumber <<
+          ": position = " << lastMeasurePosition <<
+          ", divisionsPerWholeMeasure = " <<
+          lastMeasureDivisionsPerFullMeasure <<
+        endl;
+    }
+    
+    if (fSegmentMeasuresList.size () == 1) {
+      // this is the first measure in the segment ???
+      lastMeasure->
+        setMeasureKind (
+          msrMeasure::kIncompleteLeftMeasure);
+    }
+    
+    else {
+      // this is the last measure in the segment
+      lastMeasure->
+        setMeasureKind (
+          msrMeasure::kIncompleteRightMeasure);
+    }
+  }
+
   // create the segment
   if (gGeneralOptions->fTrace)
     cerr << idtr <<
