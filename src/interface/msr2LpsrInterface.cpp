07ff4bd6e092d60e7aa5663bd6a035db03d1a991
jacques-menu <imj-219@bluewin.ch>
2017-04-01 18:20:45 +0200

    interfaces 2

---
+++ src/interface/msr2LpsrInterface.cpp
@@ -0,0 +1,152 @@
+/*
+  MusicXML Library
+  Copyright (C) Grame 2006-2013
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+  Grame Research Laboratory, 11, cours de Verdun Gensoul 69002 Lyon - France
+  research@grame.fr
+*/
+
+#ifdef VC6
+# pragma warning (disable : 4786)
+#endif
+
+#include <iostream>
+
+#include "xml.h"
+#include "xmlfile.h"
+#include "xmlreader.h"
+
+#include "versions.h"
+
+#include "msr2Lpsr.h"
+
+#include "msr2LpsrTranslator.h"
+
+using namespace std;
+
+namespace MusicXML2 
+{
+
+//_______________________________________________________________________________
+/*
+ * The method that converts the file contents to LilyPond code
+ * and  writes the result to the output stream
+*/
+S_lpsrScore msr2Lpsr (
+  const S_msrScore mScore,
+  S_msrOptions&    msrOpts,
+  S_lpsrOptions&   lpsrOpts,
+  ostream&         os) 
+{  
+  // build LPSR score from MSR score
+  S_lpsrScore
+    lpScore =
+      buildLpsrScoreFromMsrScore (
+        mScore, msrOpts, lpsrOpts, os);
+
+  // display it
+  if (lpsrOpts->fDisplayLPSR)
+    displayLpsrScore (
+      lpScore, msrOpts, lpsrOpts, cerr);
+
+  return lpScore;
+}
+
+//_______________________________________________________________________________
+S_lpsrScore buildLpsrScoreFromMsrScore (
+  const S_msrScore mScore,
+  S_msrOptions&    msrOpts,
+  S_lpsrOptions&   lpsrOpts,
+  ostream&         os)
+{
+  clock_t startClock = clock();
+  
+  if (gGeneralOptions->fTraceGeneral) {
+    string separator =
+      "%--------------------------------------------------------------";
+    
+    cerr <<
+      endl <<
+      idtr << separator <<
+      endl <<
+      "Pass 3: translating the MSR into a LPSR" <<
+      endl;
+    if (lpsrOpts->fDisplayLPSR)
+      cerr <<
+        "(dpqn: divisions per quarter note)" <<
+        endl;
+    cerr <<
+      idtr << separator <<
+      endl;
+  }
+  if (gGeneralOptions->fTraceGeneral)
+    cerr << idtr <<
+      endl;
+  
+  // create an msr2LpsrTranslator
+  msr2LpsrTranslator translator (os, mScore);
+      
+  // build the LPSR score
+  translator.buildLpsrScoreFromMsrScore ();
+
+  clock_t endClock = clock();
+
+  // register time spent
+  timing::gTiming.appendTimingItem (
+    "Pass 3: translate MSR to LPSR",
+    timingItem::kMandatory,
+    startClock,
+    endClock);
+
+  return translator.getLpsrScore ();
+}
+
+//_______________________________________________________________________________
+void displayLpsrScore (
+  const S_lpsrScore lpScore,
+  S_msrOptions&     msrOpts,
+  S_lpsrOptions&    lpsrOpts,
+  ostream&          os)
+{
+  clock_t startClock = clock();
+
+  string separator =
+    "%--------------------------------------------------------------";
+
+  if (gGeneralOptions->fTraceGeneral) {    
+    cerr <<
+      endl <<
+      idtr << separator <<
+      endl <<
+      "Optional pass: displaying the LPSR" <<
+      endl <<
+      "(dpqn: divisions per quarter note)" <<
+      endl <<
+      "(dpfm: divisions per full measure)" <<
+      endl <<
+      idtr << separator <<
+      endl;
+  }
+  
+  if (gGeneralOptions->fTraceGeneral) os << "%{" << endl;
+  os << lpScore;
+  if (gGeneralOptions->fTraceGeneral) os << "%}" << endl;
+  
+  os << separator << endl;
+
+  clock_t endClock = clock();
+
+  // register time spent
+  timing::gTiming.appendTimingItem (
+    "        display the LPSR",
+    timingItem::kOptional,
+    startClock,
+    endClock);
+}
+
+
+}
