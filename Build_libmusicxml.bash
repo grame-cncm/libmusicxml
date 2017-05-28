d714a81d9a418ff5dd8bac940eebbb1593d44be7
jacques-menu <imj-219@bluewin.ch>
2017-05-28 07:00:40 +0200

    scripts

--- Build_libmusicxml.bash
+++ Build_libmusicxml.bash
@@ -1,21 +1,69 @@
 #!/bin/bash
 
-if [ $# -eq 1 ]; then
-  DIR=$1
-else
-  DIR=.
-fi
+function usage ()
+  {
+    echo "Parameter error:"
+    echo "  '"$1"'" "is unknown"
+    echo "Usage is:"
+    echo "  $0 [cmake] [clean] [install]"
+    echo
+    exit 1
+  }
+
+CMAKE=0
+CLEAN=0
+INSTALL=0
+
+
+# Analyze the parameters if any
+# -----------------------------------------
+
+#echo '$@'" = $@"
+#echo '$#'" = $#"
+#echo
+
+for PARAM in "$@"; do
+  case $PARAM in
+    "cmake" )
+      #echo "cmake found"
+      CMAKE=1
+      ;;
+
+    "clean" )
+      #echo "clean found"
+      CLEAN=1
+      ;;
+    
+    "install" )
+      #echo "install found"
+      INSTALL=1
+      ;;
+    
+    *)
+      usage $PARAM
+      ;;
+  esac
+done
+
+echo "User choices:"
+echo "  CMAKE = $CMAKE"
+echo "  CLEAN = $CLEAN"
+echo
+
 
 # Write all output to logfile
-exec > $(basename $0).log 2>&1
+# -----------------------------------------
 
-#JMI cd ${DIR}/libmusicxml-git 
+exec > $(basename $0).log 2>&1
 
 echo
 
+
+# Let's go!
+# -----------------------------------------
+
 echo "==> date is:"
 date
-
 echo
 
 echo "==> Building libmusicxml and the samples"
@@ -27,19 +75,31 @@ cd cmake
 pwd
 echo
 
-if [ 0 -eq 1 ]; then # adapt to force running cmake after filenames changes
+
+# Should 'cmake' be run?
+# -----------------------------------------
+
+if [ $CMAKE ]; then # run cmake after filenames changes
   echo '--> cmake .'
   echo
   cmake .
   echo
 fi
 
-if [ 1 -eq 1 ]; then # adapt to force make on everything
+
+# Should 'make clean' be run?
+# -----------------------------------------
+
+if [ $CLEAN ]; then # make everything
   echo '--> make clean'
   echo
   make clean
   echo
 fi
+
+
+# Run 'make'
+# -----------------------------------------
 
 echo '--> make'
 echo
@@ -48,6 +108,10 @@ echo
 make
 #--debug=j
 echo
+
+
+# Show the results
+# -----------------------------------------
 
 echo '--> libmusicxml2'
 echo
@@ -87,18 +151,17 @@ fi
 echo
 
 
+# Should 'make install' be run?
+# -----------------------------------------
 
-exit
-
-echo '--> sudo make install'
-echo
-sudo make install
-echo
-
-echo '--> find /usr -name "*libmusicxml*"'
-echo
-find /usr -name "*libmusicxml*"
-echo
-
-
-
+if [ $INSTALL ]; then
+  echo '--> sudo make install'
+  echo
+  sudo make install
+  echo
+  
+  echo '--> find /usr -name "*libmusicxml*"'
+  echo
+  find /usr -name "*libmusicxml*"
+  echo
+fi
