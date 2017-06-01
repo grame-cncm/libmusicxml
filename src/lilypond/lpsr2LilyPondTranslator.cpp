2a762fc9505d767320b50ae27afa79696066e80e
jacques-menu <imj-219@bluewin.ch>
2017-06-01 13:29:14 +0200

    multiple rest 14

--- src/lilypond/lpsr2LilyPondTranslator.cpp
+++ src/lilypond/lpsr2LilyPondTranslator.cpp
@@ -5675,7 +5675,7 @@ void lpsr2LilyPondTranslator::visitStart (S_msrMultipleRest& elt)
     "R1" << // JMI
     "*" <<
     restMeasuresNumber <<
-    " ";
+    "| ";
 }
 
 void lpsr2LilyPondTranslator::visitEnd (S_msrMultipleRest& elt)
@@ -5684,14 +5684,6 @@ void lpsr2LilyPondTranslator::visitEnd (S_msrMultipleRest& elt)
     fOstream << idtr <<
       "% --> Start visiting msrMultipleRest" <<
       endl;
-
-  fOstream <<
-    endl <<
-    endl <<
-    idtr <<
-    " }" <<
-    endl <<
-    idtr;
     
   if (gLilypondOptions->fGenerateComments) {
     idtr--;
