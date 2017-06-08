3959ee983e68c9625ec441f8cf3e3f38c9b9f2dc
jacques-menu <imj-219@bluewin.ch>
2017-06-08 17:56:41 +0200

    putze 4

---
+++ src/interface/lpsr2LilypondInterface.cpp
@@ -0,0 +1,111 @@
+/*
+  MusicXML Library
+  Copyright (C) Grame 2006-2013
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+  Grame Research Laboratory, 11, cours de Verdun Gensoul 69002 Lyon - France
+  research@grame.fr
+*/
+
+#ifdef VC6
+# pragma warning (disable : 4786)
+#endif
+
+#include <iostream>
+
+/*
+#include "xml.h"
+#include "xmlfile.h"
+#include "xmlreader.h"
+*/
+#include "versions.h"
+
+#include "lpsr2LilypondInterface.h"
+
+#include "lpsr2LilypondTranslator.h"
+
+using namespace std;
+
+namespace MusicXML2 
+{
+
+//_______________________________________________________________________________
+/*
+ * The method that converts the file contents to LilyPond code
+ * and  writes the result to the output stream
+*/
+void lpsr2Lilypond (
+  const S_lpsrScore lpScore,
+  S_msrOptions&     msrOpts,
+  S_lpsrOptions&    lpsrOpts,
+  ostream&          os) 
+{  
+  // generate LilyPond code from LPSR score
+  generateLilypondCodeFromLpsrScore (
+    lpScore, msrOpts, lpsrOpts, os);
+}
+
+//_______________________________________________________________________________
+void generateLilypondCodeFromLpsrScore (
+  const S_lpsrScore lpScore,
+  S_msrOptions&     msrOpts,
+  S_lpsrOptions&    lpsrOpts,
+  ostream&          os)
+{
+  clock_t startClock = clock();
+
+  string separator =
+    "%--------------------------------------------------------------";
+
+  if (gGeneralOptions->fTraceGeneral) {
+    cerr <<
+      endl <<
+      idtr <<
+        separator <<
+        endl <<
+      idtr <<
+        "Pass 4: writing the LPSR as LilyPond code" <<
+        endl;
+  
+    idtr++;
+  
+    cerr <<
+      idtr <<
+        "(dpqn: divisions per quarter note)" <<
+        endl <<
+      idtr <<
+        "(dpfm: divisions per full measure)" <<
+        endl;
+  
+    idtr--;
+  
+    cerr <<
+      idtr <<
+        separator <<
+        endl;
+  }
+  
+  // create an lpsr2LilypondTranslator
+  lpsr2LilypondTranslator translator (msrOpts, lpsrOpts, os, lpScore);
+  
+  // build the LPSR score    
+  translator.generateLilypondCodeFromLpsrScore ();
+  
+  if (gGeneralOptions->fTraceGeneral)
+    os << separator << endl;
+
+  clock_t endClock = clock();
+
+  // register time spent
+  timing::gTiming.appendTimingItem (
+    "Pass 4: translate LPSR to LilyPond",
+    timingItem::kMandatory,
+    startClock,
+    endClock);
+}
+
+
+}
---
+++ src/interface/lpsr2LilypondInterface.h
@@ -0,0 +1,77 @@
+/*
+  MusicXML Library
+  Copyright (C) Grame 2006-2013
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+  Grame Research Laboratory, 11, cours de Verdun Gensoul 69002 Lyon - France
+  research@grame.fr
+*/
+
+#ifndef __lpsr2Lilypond__
+#define __lpsr2Lilypond__
+
+
+#ifdef VC6
+# pragma warning (disable : 4786)
+#endif
+
+#include <iostream>
+#include <map>
+
+#include "smartpointer.h"
+
+#include "lpsr.h"
+
+#include "lpsrOptions.h"
+
+
+namespace MusicXML2 
+{
+
+//#ifdef __cplusplus
+//extern "C" {
+//#endif
+
+/*!
+\addtogroup Converting MSR to LPSR format
+
+The library includes a high level API to convert
+  from the MusicXML format to the MSR
+  (Music Score Representation) format.
+@{
+*/
+
+//______________________________________________________________________________
+/*!
+  \brief Converts a MusicXML representation to the LilyPond format.
+  \param file a file name 
+  \param msrOpts the MSR options to be used
+  \param out the output stream
+  \return an error code (\c kNoErr when success)
+*/
+EXP void lpsr2Lilypond (
+  const S_lpsrScore lpScore,
+  S_msrOptions&     msrOpts,
+  S_lpsrOptions&    lpsrOpts,
+  ostream&          os);
+
+//______________________________________________________________________________
+void generateLilypondCodeFromLpsrScore (
+  const S_lpsrScore lpScore,
+  S_msrOptions&     msrOpts,
+  S_lpsrOptions&    lpsrOpts,
+  ostream&          os);
+
+/*! @} */
+
+//#ifdef __cplusplus
+//}
+//#endif
+
+} // namespace MusicXML2
+
+
+#endif
