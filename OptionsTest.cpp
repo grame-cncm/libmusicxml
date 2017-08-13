ae49b81747bf1816975d07fe0a6437cd77b5ad24
jacques-menu <imj-219@bluewin.ch>
2017-08-13 15:09:01 +0200

    c++ functors tests 2

--- OptionsTest.cpp
+++ OptionsTest.cpp
@@ -1,61 +1,114 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iomanip>      // setw, set::precision, ...
+
+#include <getopt.h>
 
 using namespace std;
 
-struct option
+struct myOption
 {
-  option (
+  myOption (
     string optionShortName,
     string optionLongName,
     string optionDescription)
     : fOptionShortName (optionShortName),
       fOptionLongName (optionLongName),
       fOptionDescription (optionDescription)
-    {}
+    {
+      fOptionSelected = 0;
+    }
   
   string operator() () const
     { return fOptionDescription; }
 
   void print (ostream& os) const
     {
+      const int fieldWidth = 19;
+      
       os <<
-        "fOptionShortName:" << fOptionShortName << endl <<
-        "fOptionLongName:" << fOptionLongName << endl <<
-        "fOptionDescription:" << fOptionDescription << endl;
+        setw(fieldWidth) <<
+        "fOptionShortName" << " : " << fOptionShortName <<
+        endl <<
+        setw(fieldWidth) <<
+        "fOptionLongName" << " : " << fOptionLongName <<
+        endl <<
+        setw(fieldWidth) <<
+        "fOptionDescription" << " : " << fOptionDescription <<
+        endl <<
+        setw(fieldWidth) <<
+        "fOptionSelected" << " : " << fOptionSelected <<
+        endl;
     }
 
   private:
     string fOptionShortName;
     string fOptionLongName;
     string fOptionDescription;
+
+    int    fOptionSelected;
 };
 
-ostream& operator<< (ostream& os, const option& elt)
+ostream& operator<< (ostream& os, const myOption& elt)
 {
   elt.print (os);
   return os;
 }
 
-int main()
+//_______________________________________________________________________________
+void analyzeOptions (
+  int            argc,
+  char*          argv[])
 {
-  vector<option> vec {
-    option ("1short", "1long", "descr1"),
-    option ("2short", "1long", "descr2")
+}
+
+int main (int argc, char *argv[])
+{
+  vector<myOption> vec {
+    myOption ("1short", "1long", "descr1"),
+    myOption ("2short", "1long", "descr2")
   };
 
   int counter = 0;
   cout <<
-    "The contents of 'vec' is:" << endl << endl;
-  for (option i : vec)
+    "The contents of 'vec' is:" <<
+    endl << endl;
+  for (myOption i : vec)
   {
     cout <<
-      "Element " << counter << ":" << endl <<
-      i << endl;
+      "Element " << counter << ":" <<
+      endl <<
+      i <<
+      endl;
     counter++;
   }
 
+/*
+struct option
+{
+  const char *name;
+  // has_arg can't be an enum because some compilers complain about
+  // type mismatches in all the code that assumes it is an int.
+  int has_arg;
+  int *flag;
+  int val;
+};
+*/
+
+  vector<struct option> myLongOptions {
+    option ("1short", no_argument, &vec [0].fOptionSelected, 1),
+    option ("1long", no_argument, &vec [0].fOptionSelected, 1),
+    
+    option ("2short", required_argument, &vec [1].fOptionSelected, 1),
+    option ("2long", required_argument, &vec [1].fOptionSelected, 1),
+    
+    option (0, 0, 0, 0) // option trailer
+  };
+
+
+
+/*
   cout << "Which element should be printed? ";
   
   int n;
@@ -65,10 +118,18 @@ int main()
   
   if (n < vec.size ())
     cout <<
-      "Element " << n << " constains:" << endl <<
+      "Element " << n << " constains:" <<
       endl <<
-      vec [n] << endl;
+      endl <<
+      vec [n] <<
+      endl;
   else
     cout <<
-      "Sorry, only elements from 0 to " << vec.size () - 1 << " exist" << endl;
+      "Sorry, only elements from 0 to " << vec.size () - 1 << " exist" <<
+      endl;
+*/
+
+  analyzeOptions (
+    argc, argv);
+
 }
