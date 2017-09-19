fd8610e7871053c5d981f57a9f973b02cb02ea4f
jacques-menu <imj-219@bluewin.ch>
2017-09-19 08:55:27 +0200

    options 8

---
+++ src/lilypond/msrMessages.h
@@ -0,0 +1,122 @@
+/*
+  MusicXML Library
+  Copyright (C) Grame 2006-2013
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+  Grame Research Laboratory, 11, cours de Verdun Gensoul 69002 Lyon - France
+  research@grame.fr
+*/
+
+#ifndef __lpsrOptions__
+#define __lpsrOptions__
+
+#include "lpsr.h"
+
+
+using namespace std;
+
+namespace MusicXML2 
+{
+
+//______________________________________________________________________________
+// LPSR options
+
+// trace and display
+
+#define _TRACE_LPSR_LONG_NAME_  "traceLpsr"
+#define _TRACE_LPSR_SHORT_NAME_ "tlpsr"
+
+#define _TRACE_LPSR_VISITORS_LONG_NAME_  "traceLpsrVisitors"
+#define _TRACE_LPSR_VISITORS_SHORT_NAME_ "tlpvisits"
+
+#define _DISPLAY_LPSR_LONG_NAME_  "displayLpsr"
+#define _DISPLAY_LPSR_SHORT_NAME_ "lpsr"
+
+// Scheme functions
+
+#define _TRACE_SCHEME_FUNCTIONS_LONG_NAME_  "traceSchemeFunctions"
+#define _TRACE_SCHEME_FUNCTIONS_SHORT_NAME_ "tscheme"
+
+// languages
+
+#define _LPSR_PITCHES_LANGUAGE_LONG_NAME_  "lpsrPitchesLanguage"
+#define _LPSR_PITCHES_LANGUAGE_SHORT_NAME_ "lppl"
+
+#define _LPSR_CHORDS_LANGUAGE_LONG_NAME_  "lpsrChordsLanguage"
+#define _LPSR_CHORDS_LANGUAGE_SHORT_NAME_ "lpcl"
+
+class EXP lpsrOptions : public smartable {
+  public:
+
+    static SMARTP<lpsrOptions> create ();
+    
+    SMARTP<lpsrOptions>        createCloneWithDetailedTrace ();
+
+  public:
+
+    // initialisation
+    // ------------------------------------------------------
+
+    void                  initializeLpsrOptions (
+                            bool boolOptionsInitialValue);
+    
+    void                  printLpsrOptionsHelp ();
+
+    void                  printLpsrOptionsValues (int fieldWidth);
+    
+  public:
+  
+    // constructors/destructor
+    // ------------------------------------------------------
+
+    lpsrOptions();
+    
+    virtual ~lpsrOptions();
+
+    // set and get
+    // ------------------------------------------------------
+
+    bool                  setLpsrQuarterTonesPitchesLanguage (
+                            string language);    
+ 
+    bool                  setLpsrChordsLanguage (
+                            string language);    
+ 
+  public:
+
+    // trace and display
+    // --------------------------------------
+    
+    bool                  fTraceLpsr;
+    
+    bool                  fTraceLpsrVisitors;
+    
+    bool                  fDisplayLpsr;
+    
+    // Scheme functions
+    bool                  fTraceSchemeFunctions;
+
+    // languages
+    // --------------------------------------
+
+    msrQuarterTonesPitchesLanguage
+                          fLpsrQuarterTonesPitchesLanguage;
+    
+    lpsrChordsLanguage    fLpsrChordsLanguage;
+};
+typedef SMARTP<lpsrOptions> S_lpsrOptions;
+
+extern S_lpsrOptions gLpsrOptions;
+extern S_lpsrOptions gLpsrOptionsUserChoices;
+extern S_lpsrOptions gLpsrOptionsWithDetailedTrace;
+
+
+/*! @} */
+
+}
+
+
+#endif
---
+++ src/lilypond/optionsHandling.h
@@ -0,0 +1,579 @@
+/*
+  MusicXML Library
+  Copyright (C) Grame 2006-2013
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+  Grame Research Laboratory, 11, cours de Verdun Gensoul 69002 Lyon - France
+  research@grame.fr
+*/
+
+#ifndef __msrOptionsHandling__
+#define __msrOptionsHandling__
+
+#include "list"
+#include "map"
+
+#include "smartpointer.h"
+#include "rational.h"
+
+
+using namespace std;
+
+namespace MusicXML2 
+{
+
+//______________________________________________________________________________
+class EXP msrOptionsElement : public smartable
+{
+  public:
+
+    static SMARTP<lpsrScore> create (
+      int            inputLineNumber,
+      S_msrScore     mScore);
+     
+  protected:
+
+    // constructors/destructor
+    // ------------------------------------------------------
+
+    msrOptionsElement (
+      string optionsElementShortName,
+      string optionsElementLongName,
+      string optionsElementDescription);
+
+    virtual ~msrOptionsElement();
+
+  public:
+
+    // set and get
+    // ------------------------------------------------------
+
+    string                getOptionsElementShortName () const
+                              { return fOptionsElementShortName; }
+
+    string                getOptionsElementLongName () const
+                              { return fOptionsElementLongName; }
+
+    string                getOptionsElementDescription () const
+                              { return fOptionsElementDescription; }
+
+    // services
+    // ------------------------------------------------------
+
+    string                operator() () const
+                              { return fOptionsElementDescription; }
+  
+    // print
+    // ------------------------------------------------------
+
+    virtual void          print (ostream& os) const;
+    
+  protected:
+     
+    string                fOptionsElementShortName;
+    string                fOptionsElementLongName;
+    string                fOptionsElementDescription;
+};
+typedef SMARTP<msrOptionsElement> S_msrOptionsElement;
+EXP ostream& operator<< (ostream& os, const S_msrOptionsElement& elt);
+
+//______________________________________________________________________________
+class EXP msrOptionsItem : public msrOptionsElement
+{
+  public:
+
+    // data types
+    // ------------------------------------------------------
+
+    enum msrOptionsItemKind {
+      kOptionsItemHasNoArgument,
+      kOptionsItemHasARequiredArgument,
+      kOptionsItemHasAnOptionsArgument };
+          
+    static string optionsItemKindAsString (
+      msrOptionsItemKind optionsItemKind);
+
+    static SMARTP<lpsrScore> create (
+      int            inputLineNumber,
+      S_msrScore     mScore);
+     
+  protected:
+
+    // constructors/destructor
+    // ------------------------------------------------------
+
+    msrOptionsItem (
+      string             optionsItemShortName,
+      string             optionsItemLongName,
+      string             optionsItemDescription,
+      msrOptionsItemKind optionsItemKind);
+  
+    virtual ~msrOptionsItem();
+
+  public:
+  
+    // set and get
+    // ------------------------------------------------------
+
+    msrOptionsItemKind    getOptionsItemKind () const
+                              { return fOptionsItemKind; }
+  
+    void                  setOptionsItemHasBeenSelected ()
+                              { fOptionsItemHasBeenSelected = true; }
+  
+    bool                  getOptionsItemHasBeenSelected () const
+                              { return fOptionsItemHasBeenSelected; }
+  
+    // services
+    // ------------------------------------------------------
+
+    // print
+    // ------------------------------------------------------
+
+    void                  print (ostream& os) const;
+
+  protected:
+
+    msrOptionsItemKind    fOptionsItemKind;
+    
+    bool                  fOptionsItemHasBeenSelected;
+};
+typedef SMARTP<msrOptionsItem> S_msrOptionsItem;
+EXP ostream& operator<< (ostream& os, const S_msrOptionsItem& elt);
+
+//______________________________________________________________________________
+class EXP msrOptionsBoolItem : public msrOptionsItem
+{
+  public:
+  
+    static SMARTP<lpsrScore> create (
+      int            inputLineNumber,
+      S_msrScore     mScore);
+     
+  protected:
+
+    // constructors/destructor
+    // ------------------------------------------------------
+
+    msrOptionsBoolItem (
+      string optionsItemShortName,
+      string optionsItemLongName,
+      string optionsItemDescription,
+      bool&  optionsBoolItemVariable);
+      
+    virtual ~msrOptionsBoolItem();
+
+  public:
+  
+    // set and get
+    // ------------------------------------------------------
+
+    // services
+    // ------------------------------------------------------
+
+    // print
+    // ------------------------------------------------------
+
+    void                  print (ostream& os) const;
+
+  private:
+  
+    bool&                 fOptionsBoolItemVariable;
+};
+typedef SMARTP<msrOptionsBoolItem> S_msrOptionsBoolItem;
+ostream& operator<< (ostream& os, const S_msrOptionsBoolItem& elt);
+
+//______________________________________________________________________________
+class EXP msrOptionsIntItem : public msrOptionsItem
+{
+  public:
+  
+    static SMARTP<lpsrScore> create (
+      int            inputLineNumber,
+      S_msrScore     mScore);
+     
+  protected:
+
+    // constructors/destructor
+    // ------------------------------------------------------
+
+    msrOptionsIntItem (
+      string             optionsItemShortName,
+      string             optionsItemLongName,
+      string             optionsItemDescription,
+      int&               optionsIntItemVariable,
+      msrOptionsItemKind optionsItemKind);
+          static SMARTP<lpsrScore> create (
+      int            inputLineNumber,
+      S_msrScore     mScore);
+     
+  protected:
+
+
+    virtual ~msrOptionsIntItem();
+
+  public:
+  
+    // set and get
+    // ------------------------------------------------------
+
+    // services
+    // ------------------------------------------------------
+
+    // print
+    // ------------------------------------------------------
+
+    void                  print (ostream& os) const;
+
+  private:
+  
+    int&                  fOptionsIntItemVariable;
+};
+typedef SMARTP<msrOptionsIntItem> S_msrOptionsIntItem;
+ostream& operator<< (ostream& os, const S_msrOptionsIntItem& elt);
+
+//______________________________________________________________________________
+class EXP msrOptionsFloatItem : public msrOptionsItem
+{
+  public:
+  
+    static SMARTP<lpsrScore> create (
+      int            inputLineNumber,
+      S_msrScore     mScore);
+     
+  protected:
+
+    // constructors/destructor
+    // ------------------------------------------------------
+
+    msrOptionsFloatItem (
+      string             optionsItemShortName,
+      string             optionsItemLongName,
+      string             optionsItemDescription,
+      float&             optionsFloatItemVariable,
+      msrOptionsItemKind optionsItemKind);
+      
+    virtual ~msrOptionsFloatItem();
+
+  public:
+  
+    // set and get
+    // ------------------------------------------------------
+
+    // services
+    // ------------------------------------------------------
+
+    // print
+    // ------------------------------------------------------
+
+    void                  print (ostream& os) const;
+
+  private:
+  
+    float&                fOptionsFloatItemVariable;
+};
+typedef SMARTP<msrOptionsFloatItem> S_msrOptionsFloatItem;
+ostream& operator<< (ostream& os, const S_msrOptionsFloatItem& elt);
+
+//______________________________________________________________________________
+class EXP msrOptionsStringItem : public msrOptionsItem
+{
+  public:
+  
+    static SMARTP<lpsrScore> create (
+      int            inputLineNumber,
+      S_msrScore     mScore);
+     
+  protected:
+
+    // constructors/destructor
+    // ------------------------------------------------------
+
+    msrOptionsStringItem (
+      string             optionsItemShortName,
+      string             optionsItemLongName,
+      string             optionsItemDescription,
+      string&            optionsStringItemVariable,
+      msrOptionsItemKind optionsItemKind);
+      
+    virtual ~msrOptionsStringItem();
+
+  public:
+  
+    // set and get
+    // ------------------------------------------------------
+
+    // services
+    // ------------------------------------------------------
+
+    // print
+    // ------------------------------------------------------
+
+    void                  print (ostream& os) const;
+
+  private:
+  
+    string&               fOptionsStringItemVariable;
+};
+typedef SMARTP<msrOptionsStringItem> S_msrOptionsStringItem;
+ostream& operator<< (ostream& os, const S_msrOptionsStringItem& elt);
+
+//______________________________________________________________________________
+class EXP msrOptionsRationalItem : public msrOptionsItem
+{
+  public:
+  
+    static SMARTP<lpsrScore> create (
+      int            inputLineNumber,
+      S_msrScore     mScore);
+     
+  protected:
+
+    // constructors/destructor
+    // ------------------------------------------------------
+
+    msrOptionsRationalItem (
+      string             optionsItemShortName,
+      string             optionsItemLongName,
+      string             optionsItemDescription,
+      rational&          optionsRationalItemVariable,
+      msrOptionsItemKind optionsItemKind);
+      
+    virtual ~msrOptionsRationalItem();
+
+  public:
+  
+    // set and get
+    // ------------------------------------------------------
+
+    // services
+    // ------------------------------------------------------
+
+    // print
+    // ------------------------------------------------------
+
+    void                  print (ostream& os) const;
+
+  private:
+  
+    rational&             fOptionsRationalItemVariable;
+};
+typedef SMARTP<msrOptionsRationalItem> S_msrOptionsRationalItem;
+ostream& operator<< (ostream& os, const S_msrOptionsRationalItem& elt);
+
+//_______________________________________________________________________________
+class EXP msrOptionsSubGroup : public msrOptionsElement
+{
+  public:
+  
+    static SMARTP<lpsrScore> create (
+      int            inputLineNumber,
+      S_msrScore     mScore);
+     
+  protected:
+
+    // constructors/destructor
+    // ------------------------------------------------------
+
+    msrOptionsSubGroup (
+      string optionsSubGroupShortName,
+      string optionsSubGroupLongName,
+      string optionsSubGroupDescription);
+    
+    virtual ~msrOptionsSubGroup();
+
+  public:
+  
+    // set and get
+    // ------------------------------------------------------
+
+    // services
+    // ------------------------------------------------------
+
+    void                  appendOptionsItem (
+                            S_msrOptionsItem optionsItem)
+                              {
+                                fOptionsSubGroupItemsList.push_back (
+                                  optionsItem);
+                              }
+
+    // print
+    // ------------------------------------------------------
+
+    void                  print (ostream& os) const;
+
+  private:
+  
+    list<S_msrOptionsItem>
+                          fOptionsSubGroupItemsList;
+};
+typedef SMARTP<msrOptionsSubGroup> S_msrOptionsSubGroup;
+ostream& operator<< (ostream& os, const S_msrOptionsSubGroup& elt);
+
+//_______________________________________________________________________________
+class EXP msrOptionsGroup : public msrOptionsElement
+{
+  public:
+  
+    static SMARTP<lpsrScore> create (
+      int            inputLineNumber,
+      S_msrScore     mScore);
+     
+  protected:
+
+    // constructors/destructor
+    // ------------------------------------------------------
+
+    msrOptionsGroup (
+      string optionGroupShortName,
+      string optionGroupLongName,
+      string optionGroupDescription);
+      
+    virtual ~msrOptionsGroup();
+
+  public:
+  
+    // set and get
+    // ------------------------------------------------------
+
+    // services
+    // ------------------------------------------------------
+
+    void                  appendOptionsSubGroup (
+                            S_msrOptionsSubGroup optionsSubGroup)
+                              {
+                                fOptionsGroupSubGroupsList.push_back (
+                                  optionsSubGroup);
+                              }
+        
+    // print
+    // ------------------------------------------------------
+
+    void                  print (ostream& os) const;
+
+  private:
+
+    list<S_msrOptionsSubGroup>
+                          fOptionsGroupSubGroupsList;
+};
+typedef SMARTP<msrOptionsGroup> S_msrOptionsGroup;
+ostream& operator<< (ostream& os, const S_msrOptionsGroup& elt);
+
+//_______________________________________________________________________________
+class EXP msrOptionsGroupsList : public msrOptionsElement
+{
+  public:
+  
+    static SMARTP<lpsrScore> create (
+      int            inputLineNumber,
+      S_msrScore     mScore);
+     
+  protected:
+
+    // constructors/destructor
+    // ------------------------------------------------------
+
+    msrOptionsGroupsList (
+      string optionGroupsListShortName,
+      string optionGroupsListLongName,
+      string optionGroupsListDescription);
+      
+    virtual ~msrOptionsGroupsList();
+
+  public:
+  
+    // set and get
+    // ------------------------------------------------------
+
+    // services
+    // ------------------------------------------------------
+
+    void                  appendOptionsGroup (
+                            S_msrOptionsGroup optionsGroup)
+                              {
+                                fOptionsGroupGroupsList.push_back (
+                                  optionsGroup);
+                              }
+        
+    // print
+    // ------------------------------------------------------
+
+    void                  print (ostream& os) const;
+
+  private:
+
+    list<S_msrOptionsGroup>
+                          fOptionsGroupGroupsList;
+};
+typedef SMARTP<msrOptionsGroupsList> S_msrOptionsGroupsList;
+ostream& operator<< (ostream& os, const S_msrOptionsGroupsList& elt);
+
+//_______________________________________________________________________________
+class EXP msrOptionsHandler : public msrOptionsElement
+{
+  public:
+  
+    static SMARTP<lpsrScore> create (
+      int            inputLineNumber,
+      S_msrScore     mScore);
+     
+  protected:
+
+    // constructors/destructor
+    // ------------------------------------------------------
+
+    msrOptionsHandler (
+      string optionHandlerShortName,
+      string optionHandlerLongName,
+      string optionHandlerDescription);
+      
+    virtual ~msrOptionsHandler();
+
+  public:
+  
+    // services
+    // ------------------------------------------------------
+
+    void                  checkOptionUniqueness (
+                            string optionLongName,
+                            string optionShortName);
+
+    void                  appendOptionsGroup (
+                            S_msrOptionsGroup optionsGroup)
+                              {
+                                fOptionsGroupHandler.push_back (
+                                  optionsGroup);
+                              }
+        
+    void                  analyzeOptions (
+                            int   argc,
+                            char* argv[]);
+
+    // print
+    // ------------------------------------------------------
+
+    void                  print (ostream& os) const;
+
+  private:
+
+    list<S_msrOptionsGroup>
+                          fOptionsGroupHandler;
+
+    map<string, string>   fOptionShortNames;
+};
+typedef SMARTP<msrOptionsHandler> S_msrOptionsHandler;
+ostream& operator<< (ostream& os, const S_msrOptionsHandler& elt);
+
+//______________________________________________________________________________
+void initializeOptionsHandling ();
+
+
+/*! @} */
+
+}
+
+
+#endif
