7eab90cb4c179bf46de1564e6f4268fb0ac0d521
jacques-menu <imj-219@bluewin.ch>
2017-10-01 12:18:10 +0200

    options 15

--- src/lilypond/msr.cpp
+++ src/lilypond/msr.cpp
@@ -16541,6 +16541,21 @@ void msrMeasure::finalizeMeasure (
     idtr--;
   }
 
+  if (fMeasureLength.getNumerator () == 0) { // JMI
+    // empty measure
+    if (gGeneralOptions->fTraceMeasures) {
+      cerr << idtr <<
+      "Measure '" << fMeasureNumber <<
+      "' in voice \"" << voice->getVoiceName () <<
+      "\", is **empty**" <<
+      ", line " << inputLineNumber <<
+      endl;
+    }
+
+    abort (); // JMI
+  }
+    
+
   if (fMeasureKind != msrMeasure::kSenzaMisuraMeasureKind) {
     
     if (fMeasureLength < partMeasureLengthHighTide) {
@@ -16611,7 +16626,7 @@ void msrMeasure::finalizeMeasure (
         kFullMeasureKind);
     }
     
-    else if (fMeasureLength.getNumerator () == 0) {
+    else if (fMeasureLength.getNumerator () == 0) { // JMI
       // empty measure
       if (gGeneralOptions->fTraceMeasures) {
         cerr << idtr <<
