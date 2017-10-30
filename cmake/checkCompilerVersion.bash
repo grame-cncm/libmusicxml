d5b33588109a1e246f87b3215beb25f307e3580a
jacques-menu <imj-219@bluewin.ch>
2017-10-30 11:40:23 +0100

    part groups 17

--- cmake/checkCompilerVersion.bash
+++ cmake/checkCompilerVersion.bash
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+set -x
+
 g++ \
   -Wall -DGCC -std=c++11 -ggdb3 \
-  -o compilerVersionTe\
-  compilerVersionTest.cpp
+  -o compilerVersionCheck \
+  compilerVersionCheck.cpp
 
-./compilerVersionTest
+echo
+
+./compilerVersionCheck
+
+echo
