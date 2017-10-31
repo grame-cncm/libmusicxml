7acefbe1d03ec1802f69be9ce4b122bcbabbacb8
D. Fober <fober@grame.fr>
2017-10-31 18:42:20 +0100

    remove using namespace in header file fix clock_t type + corresponding header

--- src/lilypond/utilities.h
+++ src/lilypond/utilities.h
@@ -21,14 +21,11 @@
 
 #include <set>
 #include <list>
-#include <algorithm> 
-
+#include <algorithm>
+#include <ctime>
 
 #include "smartpointer.h"
-
 #include "basevisitor.h"
-
-using namespace std;
 
 namespace MusicXML2 
 {
@@ -47,60 +44,54 @@ namespace MusicXML2
 class timingItem : public smartable
 {
   public:
-
-    enum timingItemKind {
-      kMandatory, kOptional };
+    enum timingItemKind { kMandatory, kOptional };
       
     static SMARTP<timingItem> createTimingItem (
-      string         activity,
-      string         description,
+      std::string    activity,
+      std::string    description,
       timingItemKind kind,
-      clock_t        startClock,
-      clock_t        endClock);
+      std::clock_t   startClock,
+      std::clock_t   endClock);
 
     timingItem (
-      string         activity,
-      string         description,
+      std::string    activity,
+      std::string    description,
       timingItemKind kind,
-      clock_t        startClock,
-      clock_t        endClock);
+      std::clock_t   startClock,
+      std::clock_t   endClock);
       
-    string                fActivity;
-    string                fDescription;
-    timingItemKind        fKind;
-    clock_t               fStartClock;
-    clock_t               fEndClock;
+    std::string  	fActivity;
+    std::string 	fDescription;
+    timingItemKind	fKind;
+    std::clock_t 	fStartClock;
+    std::clock_t  	fEndClock;
 };
 
 typedef SMARTP<timingItem> S_timingItem;
 
 class timing {
   public:
-
-    timing ();
-      
+			 timing ();
     virtual ~timing ();
 
     // global variable for general use
     static timing gTiming; 
 
     // add an item
-    void                  appendTimingItem (
-                            string         activity,
-                            string         description,
-                            timingItem::timingItemKind
-                                           kind,
-                            clock_t        startClock,
-                            clock_t        endClock);
+    void    appendTimingItem (
+						std::string    activity,
+						std::string    description,
+						timingItem::timingItemKind kind,
+						std::clock_t   startClock,
+						std::clock_t   endClock);
       
     // print
-    void                  print (ostream& os) const;
+    void    print (std::ostream& os) const;
 
   private:
-
-    list<S_timingItem>    fTimingItemsList;
+    std::list<S_timingItem>    fTimingItemsList;
 };
-ostream& operator<< (ostream& os, const timing& tim);
+std::ostream& operator<< (std::ostream& os, const timing& tim);
 
 //______________________________________________________________________________
 /*!
@@ -110,13 +101,11 @@ ostream& operator<< (ostream& os, const timing& tim);
 class indenter
 {
   public:
-
-    indenter (string spacer = "  ");
+			  indenter (std::string spacer = "  ");
     virtual ~indenter ();
 
     // get the indent
-    int                   getIndent () const
-                              { return fIndent; }
+    int                   getIndent () const 	{ return fIndent; }
                          
     // increase the indentation by 1
     indenter&             operator++ (const int value);
@@ -128,35 +117,32 @@ class indenter
     indenter&             decrement (int value);
 
     // reset the indentation
-    void                  resetToZero ()
-                              { fIndent = 0; }
+    void                  resetToZero ()		{ fIndent = 0; }
 
     // output as much space as specified
-    void                  print (ostream& os) const;
+    void                  print (std::ostream& os) const;
 
     // get a spacer for adhoc uses, without increasing the indentation
-    string                getSpacer () const
-                              { return fSpacer; }
+    std::string           getSpacer () const	{ return fSpacer; }
 
     // indent a multiline 'R"(...)"' string
-    string                indentMultiLineString (string value);
+    std::string                indentMultiLineString (std::string value);
     
     // global variable for general use
     static indenter       gIndenter; 
 
   private:
-
     int                   fIndent;
-    string                fSpacer;
+    std::string           fSpacer;
 };
-ostream& operator<< (ostream& os, const indenter& idtr);
+std::ostream& operator<< (std::ostream& os, const indenter& idtr);
 
 // useful shortcut macros
 #define gIndenter indenter::gIndenter
 #define gTab      indenter::gIndenter.getSpacer ()
 
 //______________________________________________________________________________
-class indentedOstream: public ostream
+class indentedOstream: public std::ostream
 {
 /*
 Reference:
@@ -173,28 +159,18 @@ Usage:
  
   // a stream buffer that prefixes each line
   // with the current indentation
-  class indentedStreamBuf: public stringbuf
+  class indentedStreamBuf: public std::stringbuf
   {
     private:
-    
-      ostream&  fOutput;
-      indenter& fIndenter;
+      std::ostream&  fOutput;
+      indenter&      fIndenter;
 
     public:
-
       // constructor
-      indentedStreamBuf (
-        ostream& str,
-        indenter& idtr)
-        : fOutput (str),
-          fIndenter (idtr)
-        {}
+      indentedStreamBuf ( std::ostream& str, indenter& idtr) : fOutput (str), fIndenter (idtr)	{}
 
       // flush
-      void flush ()
-        {
-          fOutput.flush ();
-        }
+      void flush ()		{ fOutput.flush (); }
     
       // When we sync the stream with fOutput:
       // 1) uutput the indentation then the buffer
@@ -202,44 +178,31 @@ Usage:
       // 3) flush the actual output stream we are using.
       virtual int sync ()
       {
-        fOutput <<
-          fIndenter << str ();
-          
+        fOutput << fIndenter << str ();
         str ("");
-        
         fOutput.flush ();
-        
         return 0;
       }
   };
 
   private:
-  
     // indentedOstream just uses a version of indentedStreamBuf
     indentedStreamBuf     fIndentedStreamBuf;
   
   public:
-
     // constructor
-    indentedOstream (
-      ostream&  str  = cerr,
-      indenter& idtr = indenter::gIndenter)
-      : ostream (&fIndentedStreamBuf),
-        fIndentedStreamBuf (
-          str, idtr)
+    indentedOstream ( std::ostream&  str  = std::cerr, indenter& idtr = indenter::gIndenter)
+      : std::ostream (&fIndentedStreamBuf), fIndentedStreamBuf (str, idtr)
       {}
 
     // destructor
-    virtual ~indentedOstream ()
-      {};
+    virtual ~indentedOstream () {};
 
     // flush
-    void flush ()
-      { fIndentedStreamBuf.flush (); }
+    void flush () 			{ fIndentedStreamBuf.flush (); }
     
     // global variable for general use
-    static indentedOstream
-                          gLogIndentedOstream; 
+    static indentedOstream gLogIndentedOstream;
 };
 
 // useful shortcut macro
@@ -255,7 +218,7 @@ class outputLineElementsCounter
   public:
 
     outputLineElementsCounter (
-      ostream& ostream,
+      std::ostream& ostream,
       int      maxElementsPerLine = 10);
       
     virtual ~outputLineElementsCounter ();
@@ -303,10 +266,7 @@ class outputLineElementsCounter
                           gOutputLineElementsCounter; 
 
   private:
-
-    ostream&              fOstream;
-
-
+    std::ostream&         fOstream;
     int                   fElementsCounter;
     int                   fMaxElementsPerLine;
 };
@@ -322,9 +282,9 @@ struct stringQuoteEscaper
       for_each( source.begin (), source.end (), stringQuoteEscaper (dest));
   */
 
-  string&                 target;
+  std::string&            target;
   
-  explicit                stringQuoteEscaper (string& t) : target (t) {}
+  explicit                stringQuoteEscaper (std::string& t) : target (t) {}
 
   void                    operator() (char ch) const
                               {
@@ -351,9 +311,9 @@ struct stringSpaceRemover
         stringSpaceRemover (dest));
   */
 
-  string&                 target;
+  std::string&            target;
   
-  explicit                stringSpaceRemover (string& t)
+  explicit                stringSpaceRemover (std::string& t)
                             : target (t)
                             {}
 
@@ -379,10 +339,10 @@ struct stringSpaceReplacer
         stringSpaceReplacer (dest, ersatz));
   */
 
-  string&                 target;
+  std::string&            target;
   char                    ersatz;
   
-  explicit                stringSpaceReplacer (string& t, char ch)
+  explicit                stringSpaceReplacer (std::string& t, char ch)
                             : target (t), ersatz (ch)
                             {}
 
@@ -399,21 +359,19 @@ struct stringSpaceReplacer
 \brief A utility to convert small positive integers to English words.
 */
 //______________________________________________________________________________
-string replicateString (
-  string str,
-  int    times);
+std::string replicateString ( std::string str, int times);
 
 /*!
 \brief A utility to convert small positive integers to English words.
 */
 //______________________________________________________________________________
-string int2EnglishWord (int n);
+std::string int2EnglishWord (int n);
 
 /*!
 \brief A utility to escape quotes in strings.
 */
 //______________________________________________________________________________
-string stringNumbersToEnglishWords (string str);
+std::string stringNumbersToEnglishWords (std::string str);
 
 /*!
 \brief A utility to build a list of number from a specification.
@@ -422,85 +380,62 @@ string stringNumbersToEnglishWords (string str);
     meaning that 7, 15, 18 and 19 have to enlisted
 */
 //______________________________________________________________________________
-set<int> decipherNumbersSetSpecification (
-  string theSpecification,
-  bool   debugMode = false);
+std::set<int> decipherNumbersSetSpecification ( std::string theSpecification, bool   debugMode = false);
 
 //______________________________________________________________________________
-list<int> extractNumbersFromString (
-  string theString, // can contain "1, 2, 17"
+std::list<int> extractNumbersFromString (
+  std::string theString, // can contain "1, 2, 17"
   bool   debugMode = false);
 
 //______________________________________________________________________________
 // from http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
 // trim string from start
-inline string &ltrim (string &s) {
-  s.erase (
-    s.begin (),
-    find_if (
-      s.begin (),
-      s.end (),
-      not1 (ptr_fun<int, int> (isspace))
-      )
-    );
-          
+inline std::string &ltrim (std::string &s) {
+  s.erase ( s.begin (), find_if (s.begin (), s.end (), not1 (std::ptr_fun<int, int> (isspace))));
   return s;
 }
 
 // trim string from end
-inline string &rtrim (string &s) {
-  s.erase (
-    find_if (
-      s.rbegin (),
-      s.rend (),
-      not1 (ptr_fun<int, int> (isspace))
-      ).base(),
-    s.end ()
-    );
-          
+inline std::string &rtrim (std::string &s) {
+  s.erase ( find_if (s.rbegin (), s.rend (), not1 (std::ptr_fun<int, int> (isspace)) ).base(), s.end () );
   return s;
 }
 
 // trim string from both ends
-inline string &trim (string &s) {
+inline std::string &trim (std::string &s) {
   return ltrim (rtrim (s));
 }
 
 //______________________________________________________________________________
-pair<string, string> extractNamesPairFromString (
-  string theString, // can contain "P1 = Bassoon"
+std::pair<std::string, std::string> extractNamesPairFromString (
+  std::string theString, // can contain "P1 = Bassoon"
   char   separator,
   bool   debugMode = false);
 
 //______________________________________________________________________________
-string quoteStringIfNonAlpha (
-  string theString);
-
-string quoteString (
-  string theString);
+std::string quoteStringIfNonAlpha (std::string theString);
+std::string quoteString (std::string theString);
 
 //______________________________________________________________________________
-string booleanAsString (bool value);
+std::string booleanAsString (bool value);
 
 //______________________________________________________________________________
-string singularOrPlural (
-  int number, string singularName, string pluralName);
+std::string singularOrPlural (int number, std::string singularName, std::string pluralName);
 
-string singularOrPluralWithoutNumber (
-  int number, string singularName, string pluralName);
+std::string singularOrPluralWithoutNumber ( int number, std::string singularName, std::string pluralName);
 
 //______________________________________________________________________________
-void optionError (string errorMessage);
+void optionError (std::string errorMessage);
 
 //______________________________________________________________________________
-void convertHTMLEntitiesToPlainCharacters (string& s);
+void convertHTMLEntitiesToPlainCharacters (std::string& s);
 
 //______________________________________________________________________________
-string baseName (const string &filename);
+std::string baseName (const std::string &filename);
   // wait until c++17 for a standard library containing basename()...
 
 //______________________________________________________________________________
-string makeSingleWordFromString (const string &string);
+std::string makeSingleWordFromString (const std::string &string);
 
 
 } // namespace MusicXML2
