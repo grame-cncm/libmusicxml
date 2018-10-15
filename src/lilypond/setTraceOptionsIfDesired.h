8a20ff07d0d7cfb762ac588af2a928739427c055
jacques-menu <imj-219@bluewin.ch>
2018-10-15 19:26:27 +0200

    Moving TRACE_OPTIONS to the xml2ly side

---
+++ src/lilypond/setTraceOptionsIfDesired.h
@@ -0,0 +1,21 @@
+/*
+  MusicXML Library
+  Copyright (C) Grame 2006-2013
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+  Grame Research Laboratory, 11, cours de Verdun Gensoul 69002 Lyon - France
+  research@grame.fr
+*/
+
+#ifndef ___setTraceOptionsIfDesired___
+#define ___setTraceOptionsIfDesired___
+
+
+// comment the following definition if no trace options are wanted
+#define TRACE_OPTIONS
+
+
+#endif
