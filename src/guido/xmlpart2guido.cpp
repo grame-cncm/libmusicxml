b39fbbdb4efb3f7ec48cd2ab5699727d388f1d52
Arshia Cont <arshiacont@antescofo.com>
2020-01-09 16:11:04 +0100

    xml2guido: Fixed ordering of grace note tag creation + added unit test

--- src/guido/xmlpart2guido.cpp
+++ src/guido/xmlpart2guido.cpp
@@ -2221,6 +2221,8 @@ void xmlpart2guido::checkPostArticulation ( const notevisitor& note )
     {
         if (nv.isGrace()) {
             if (!fInGrace) {
+                Sguidoelement tag = guidotag::create("grace");
+                push(tag);
                 /// GUID-153: Fetch directions after grace
                 ctree<xmlelement>::iterator nextnote = find(fCurrentMeasure->begin(), fCurrentMeasure->end(), nv.getSnote());
                 nextnote.forward_up(); // forward one element
@@ -2251,8 +2253,6 @@ void xmlpart2guido::checkPostArticulation ( const notevisitor& note )
                 }
                 /// End-of Guid-153
                 fInGrace = true;
-                Sguidoelement tag = guidotag::create("grace");
-                push(tag);
             }
         }
         else if (fInGrace) {
