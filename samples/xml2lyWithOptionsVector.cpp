ded6641a6e991247d85c9747103ab83fd304232a
Jacques Menu <imj-muzhic@bluewin.ch>
2020-05-13 23:17:24 +0200

    Restructured the code base hierarchy, phase 2

---
+++ samples/xml2lyWithOptionsVector.cpp
@@ -0,0 +1,293 @@
+/*
+
+  Copyright (C) 2003-2008  Grame
+  Grame Research Laboratory, 9 rue du Garet, 69001 Lyon - France
+  research@grame.fr
+
+  This file is provided as an example of the MusicXML Library use.
+*/
+
+#include <iomanip>      // setw()), set::precision(), ...
+
+#ifndef WIN32
+#include <signal.h>
+#endif
+
+#include "libmusicxml.h"
+#include "version.h"
+
+#include "utilities.h"
+
+#include "setTraceOahIfDesired.h"
+#ifdef TRACE_OAH
+  #include "traceOah.h"
+#endif
+
+#include "generalOah.h"
+#include "mxmlTreeOah.h"
+#include "msr2LpsrOah.h"
+
+#include "xml2lyOah.h"
+
+#include "musicXML2MxmlTreeInterface.h"
+
+#include "mxmlTree2MsrSkeletonBuilderInterface.h"
+#include "mxmlTree2MsrTranslatorInterface.h"
+
+#include "msr2LpsrInterface.h"
+
+#include "lpsr2LilypondInterface.h"
+
+#include "msr2MxmltreeInterface.h"
+
+#include "mxmlTree.h"
+
+
+using namespace std;
+
+using namespace MusicXML2;
+
+
+//_______________________________________________________________________________
+#ifndef WIN32
+
+static void _sigaction(int signal, siginfo_t *si, void *arg)
+{
+    cerr << "Signal #" << signal << " catched!" << endl;
+    exit(-2);
+}
+
+static void catchsigs()
+{
+	struct sigaction sa;
+
+    memset (&sa, 0, sizeof(struct sigaction));
+
+    sigemptyset (&sa.sa_mask);
+
+    sa.sa_sigaction = _sigaction;
+    sa.sa_flags     = SA_SIGINFO;
+
+    sigaction (SIGSEGV, &sa, NULL);
+    sigaction (SIGILL, &sa, NULL);
+    sigaction (SIGFPE, &sa, NULL);
+}
+
+#else
+static void catchsigs()	{}
+#endif
+
+//_______________________________________________________________________________
+void displayMsrScore_OptionalPass (
+  S_msrScore mScore,
+  S_msrOah   msrOpts)
+{
+  // display the MSR
+  displayMSRPopulatedScore (
+    msrOpts,
+    mScore,
+    gLogOstream);
+
+  if (gIndenter != 0) {
+    if (! gGeneralOah->fQuiet) {
+      stringstream s;
+
+      s <<
+        "gIndenter value after MSR score display: "<<
+        gIndenter.getIndent ();
+
+      msrMusicXMLWarning (
+        gOahOah->fInputSourceName,
+        1, // JMI inputLineNumber,
+        s.str ());
+    }
+
+    gIndenter.resetToZero ();
+  }
+}
+
+//_______________________________________________________________________________
+int main (int argc, char *argv[])
+{
+  // create the signals
+  // ------------------------------------------------------
+
+	catchsigs();
+
+  // create the options handler
+  // ------------------------------------------------------
+
+  S_xml2lyOahHandler
+    handler =
+      xml2lyOahHandler::create (
+        argv [0],
+        gOutputOstream);
+
+  // analyze the command line options and arguments
+  // ------------------------------------------------------
+
+  vector<string>
+    argumentsVector =
+      handler->
+        applyOptionsAndArgumentsFromArgcAndArgv (
+          argc, argv);
+
+  string
+    inputSourceName =
+      gOahOah->fInputSourceName;
+
+  string
+    outputFileName =
+      gXml2lyOah->fLilyPondOutputFileName;
+
+  int
+    outputFileNameSize =
+      outputFileName.size ();
+
+
+  if (true) { // JMI, TEST
+  /*
+    xml2lilypond (
+      argc,
+      argv,
+      cout,
+      inputSourceName);
+  }
+
+  else {
+  */
+  // has quiet mode been requested?
+  // ------------------------------------------------------
+
+  if (gGeneralOah->fQuiet) {
+    // disable all trace and display options
+    handler->
+      enforceOahHandlerQuietness ();
+  }
+
+  // welcome message
+  // ------------------------------------------------------
+
+#ifdef TRACE_OAH
+  if (gTraceOah->fTracePasses) {
+    gLogOstream <<
+      "This is xml2ly " << currentVersionNumber () <<
+      " from libmusicxml2 v" << musicxmllibVersionStr () <<
+      endl;
+
+    gLogOstream <<
+      "Launching conversion of ";
+
+    if (inputSourceName == "-")
+      gLogOstream <<
+        "standard input";
+    else
+      gLogOstream <<
+        "\"" << inputSourceName << "\"";
+
+    gLogOstream <<
+      " to LilyPond" <<
+      endl;
+
+    gLogOstream <<
+      "Time is " << gGeneralOah->fTranslationDateFull <<
+      endl;
+
+    gLogOstream <<
+      "LilyPond code will be written to ";
+    if (outputFileNameSize) {
+      gLogOstream <<
+        outputFileName;
+    }
+    else {
+      gLogOstream <<
+        "standard output";
+    }
+    gLogOstream <<
+      endl <<
+      endl;
+
+    gLogOstream <<
+      "The command line is:" <<
+      endl;
+
+    gIndenter++;
+
+    gLogOstream <<
+      handler->
+        commandLineWithShortNamesAsString () <<
+      endl;
+
+    gIndenter--;
+    gLogOstream <<
+      "or:" <<
+      endl;
+    gIndenter++;
+
+    gLogOstream <<
+      handler->
+        commandLineWithLongNamesAsString () <<
+      endl <<
+      endl;
+
+    gIndenter--;
+  }
+#endif
+
+  // acknoledge end of command line analysis
+  // ------------------------------------------------------
+
+#ifdef TRACE_OAH
+  if (gTraceOah->fTracePasses) {
+    gLogOstream <<
+      "The command line options and arguments have been analyzed" <<
+      endl;
+  }
+#endif
+
+  // do the translation
+  // ------------------------------------------------------
+
+  convertMusicXMLToLilypond (
+    inputSourceName,
+    outputFileName);
+
+  // display the input line numbers for which messages have been issued
+  // ------------------------------------------------------
+
+  displayWarningsAndErrorsInputLineNumbers ();
+
+  // print timing information
+  // ------------------------------------------------------
+
+  if (gGeneralOah->fDisplayCPUusage)
+    timing::gTiming.print (
+      gLogOstream);
+
+  // check indentation
+  // ------------------------------------------------------
+  if (gIndenter != 0) {
+    gLogOstream <<
+      "### gIndenter final value: "<< gIndenter.getIndent () << " ###" <<
+      endl <<
+      endl;
+
+    // JMI abort ();
+  }
+
+  // over!
+  // ------------------------------------------------------
+
+  if (! true) { // JMI
+    gLogOstream <<
+      "### Conversion from LPSR to LilyPond code failed ###" <<
+      endl <<
+      endl;
+
+    return 1;
+  }
+}
+
+  return 0;
+}
+
