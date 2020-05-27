29025e438c3cd0c636053878b23dc5969ac9d6a1
Dominique Fober <fober@grame.fr>
2020-05-27 07:46:58 +0200

    update to lilypond and braille conversions

--- javascript/npm/libmusicxml.ts
+++ javascript/npm/libmusicxml.ts
@@ -32,6 +32,15 @@ class libmusicxml {
 	musicxml2guidoVersion ()    : number			{ return this.fLibrary.musicxml2guidoVersion (); }
 	musicxml2guidoVersionStr ()	: string			{ return this.fLibrary.musicxml2guidoVersionStr (); }
 
+    musicxml2lilypondVersion(): number				{ return this.fLibrary.musicxml2lilypondVersion (); }
+    musicxml2lilypondVersionStr(): string			{ return this.fLibrary.musicxml2lilypondVersionStr (); }
+
+    musicxml2brailleVersion(): number				{ return this.fLibrary.musicxml2brailleVersion (); }
+    musicxml2brailleVersionStr(): string			{ return this.fLibrary.musicxml2brailleVersionStr (); }
+
 	string2guido (xml: string, genBars: boolean)		{ return this.fLibrary.string2guido (xml, genBars); }
 	xmlStringTranspose (xml: string, interval: number)	{ return this.fLibrary.xmlStringTranspose (xml, interval); }
+
+	string2lily (xml: string, options: string): 	string		{ return this.fLibrary.string2lily (xml, options); }
+	string2braille (xml: string, options: string): 	string		{ return this.fLibrary.string2braille (xml, options); }
 }
